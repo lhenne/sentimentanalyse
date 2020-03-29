1b90f3e8c0de533345c2dd77cfccd6e2a300853b
lhenne <lhenne@posteo.de>
2020-03-29 21:34:40 +0200

    changed result format for dictionary-based analysis, non-destructive application of stopword list

--- dictionary_analysis.py
+++ dictionary_analysis.py
@@ -29,6 +29,7 @@ def collect_classifiers(sourcefile, target_dict):
 def drop_stopwoerter(protokoll):
     for i in range(len(protokoll)):
         rede = protokoll[i]
+        rede["inhalt"]["tokenisiert_voll"] = [x for x in rede["inhalt"]["tokenisiert"]]
         for j in range(len(rede["inhalt"]["tokenisiert"])):
             absatz = rede["inhalt"]["tokenisiert"][j]
             rede["inhalt"]["tokenisiert"][j] = [wort for wort in absatz if wort not in deutsche_stopwoerter]
@@ -36,29 +37,40 @@ def drop_stopwoerter(protokoll):
     return protokoll
 
 
-def sentiment(input_text):
+def analyse(log):
+    protokoll_datei = open(log, "r+").read()
+    protokoll_objekt = drop_stopwoerter(ast.literal_eval(protokoll_datei))
+
+    for i in range(len(protokoll_objekt)):
+        rede_tokenisiert = protokoll_objekt[i]["inhalt"]["tokenisiert"]
+        rede_tokenisiert_voll = protokoll_objekt[i]["inhalt"]["tokenisiert_voll"]
+        protokoll_objekt[i]["meta"]["sentimentwerte"] = []
+        protokoll_objekt[i]["meta"]["wortanzahl"] = []
+        for j in range(len(rede_tokenisiert)):
+            absatz = rede_tokenisiert[j]
+            scores_dict = {}
+            for wort in absatz:
+                scores_dict[wort] = token_sentiment(wort)
+            absatz = scores_dict
+            rede_tokenisiert[j] = absatz
+            protokoll_objekt[i]["meta"]["sentimentwerte"].append(sum(absatz.values()))
+            protokoll_objekt[i]["meta"]["wortanzahl"].append(len(rede_tokenisiert_voll[j]))
+        protokoll_objekt[i]["inhalt"]["tokenisiert"] = rede_tokenisiert
+    return protokoll_objekt
+
+
+def token_sentiment(token):
     score = 0
-    postokens = 0
-    negtokens = 0
-    score_postokens = 0
-    score_negtokens = 0
-    for token in input_text:
-        if token in classifiers_pos:
-            postokens += 1
-            score_postokens += classifiers_pos[token]
-            score += classifiers_pos[token]
-        if token in classifiers_neg:
-            negtokens += 1
-            score_negtokens -= classifiers_neg[token]
-            score += classifiers_neg[token]
-    return score/len(input_text)
+    if token in classifiers_pos:
+        score = classifiers_pos[token]
+    elif token in classifiers_neg:
+        score = classifiers_neg[token]
+    return score
 
 
 collect_classifiers("SentiWS/SentiWS_v2.0_Negative.txt", classifiers_neg)
 collect_classifiers("SentiWS/SentiWS_v2.0_Positive.txt", classifiers_pos)
 
-for log in daten:
-    protokoll_datei = open(log, "r+").read()
-    protokoll_objekt = ast.literal_eval(protokoll_datei)
-    protokoll_objekt = drop_stopwoerter(protokoll_objekt)
-    breakpoint()
+for datei in daten:
+    datei_mit_werten = analyse(datei)
+
