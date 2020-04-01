0191a7135618e9b30991a85232d1f844ac31df48
lhenne <lhenne@posteo.de>
2020-04-01 17:02:51 +0200

    completed data collection from sentiment lexicons and included it in dictionary_analysis.py, removed unnecessary file

--- dictionary_analysis.py
+++ dictionary_analysis.py
@@ -1,75 +1,107 @@
-import csv
+from pandas import read_csv
+from glob import glob
 import spacy
 from spacy.tokens import DocBin
-from glob import glob
 
 nlp = spacy.load("de_core_news_sm")
+
 daten = glob("plenarprotokolle/testing_prep/*.xml.spacy")
 
-sprecher_werte = {}
-classifiers_neg, classifiers_pos = {}, {}
 
-def collect_classifiers(sourcefile, target_dict):
-    with open(sourcefile) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter="\t")
-        line_count = 0
-        for row in csv_reader:
-            wert = float(row[1])
-            formen = row[2].split(',')
-            formen.append(row[0])
-            # Wortart-Bezeichner vom Wort abtrennen:
-            for form in formen:
-                if '|' in form:
-                    form = form.split('|')[0]
-                target_dict[form.lower()] = wert
-            line_count += 1
-
-
-def drop_stopwoerter(protokoll):
-    for i in range(len(protokoll)):
-        rede = protokoll[i]
-        rede["inhalt"]["tokenisiert_voll"] = [x for x in rede["inhalt"]["tokenisiert"]]
-        for j in range(len(rede["inhalt"]["tokenisiert"])):
-            absatz = rede["inhalt"]["tokenisiert"][j]
-            rede["inhalt"]["tokenisiert"][j] = [wort for wort in absatz if wort not in deutsche_stopwoerter]
-        protokoll[i] = rede
-    return protokoll
-
-
-def analyse(log):
-    protokoll_datei = open(log, "r+").read()
-    protokoll_objekt = ast.literal_eval(protokoll_datei)
-
-    for i in range(len(protokoll_objekt)):
-        rede_tokenisiert = protokoll_objekt[i]["inhalt"]["tokenisiert"]
-        rede_tokenisiert_voll = protokoll_objekt[i]["inhalt"]["tokenisiert_voll"]
-        protokoll_objekt[i]["meta"]["sentimentwerte"] = []
-        protokoll_objekt[i]["meta"]["wortanzahl"] = []
-        for j in range(len(rede_tokenisiert)):
-            absatz = rede_tokenisiert[j]
-            scores_dict = {}
-            for wort in absatz:
-                scores_dict[wort] = token_sentiment(wort)
-            absatz = scores_dict
-            rede_tokenisiert[j] = absatz
-            protokoll_objekt[i]["meta"]["sentimentwerte"].append(sum(absatz.values()))
-            protokoll_objekt[i]["meta"]["wortanzahl"].append(len(rede_tokenisiert_voll[j]))
-        protokoll_objekt[i]["inhalt"]["tokenisiert"] = rede_tokenisiert
-    return protokoll_objekt
-
-
-def token_sentiment(token):
-    score = 0
-    if token in classifiers_pos:
-        score = classifiers_pos[token]
-    elif token in classifiers_neg:
-        score = classifiers_neg[token]
-    return score
-
-
-collect_classifiers("SentiWS/SentiWS_v2.0_Negative.txt", classifiers_neg)
-collect_classifiers("SentiWS/SentiWS_v2.0_Positive.txt", classifiers_pos)
-
+spacy_db = {}
 for datei in daten:
-    datei_mit_werten = analyse(datei)
+    protokoll = DocBin(store_user_data=True).from_bytes(open(datei, "rb").read())
+    protokoll = list(protokoll.get_docs(nlp.vocab))
+    datei = datei.lstrip("plenarprotokolle/testing_prep/")
+    spacy_db[datei] = protokoll
+
+for f, protokoll in spacy_db.items():
+    for rede in protokoll:
+        rede.user_data["entitaeten"] = [x.text for x in rede.ents]
+        rede.user_data["entitaeten"] = [x for x in rede.user_data["entitaeten"] if not x == "||"]
+
+
+def collect_classifiers_sentiws(sourcefile):
+    with open(sourcefile) as csv_file:
+        classifiers = read_csv(csv_file, sep="\t", header=None, names=["lemma", "wert", "formen"])
+        classifiers["formen"] = classifiers["formen"].astype(str)
+        classifiers["formen"] = classifiers["formen"].apply(lambda x: x.split(","))
+        classifiers[["lemma", "pos"]] = classifiers["lemma"].str.split("|", expand=True)
+        classifiers["lemma"] = classifiers["lemma"].astype(str)
+        for formen, lemma in zip(classifiers.formen, classifiers.lemma):
+            formen = formen.append(lemma)
+        classifiers = classifiers.explode("formen")
+    return classifiers
+
+
+def collect_classifiers_gpc(sourcefile):
+    with open(sourcefile) as csv_file:
+        classifiers = read_csv(csv_file, sep="\t", header=None,
+                               names=["form", "lemma", "pos", "polaritaet", "probabilitaet", "whatever"])
+        classifiers = classifiers.drop(labels=["probabilitaet", "whatever"], axis=1)
+    return classifiers
+
+
+classifiers_neg_sentiws = collect_classifiers_sentiws("SentiWS/SentiWS_v2.0_Negative.txt")
+classifiers_pos_sentiws = collect_classifiers_sentiws("SentiWS/SentiWS_v2.0_Positive.txt")
+
+classifiers_neg_gpc = collect_classifiers_gpc("gpc/GermanPolarityClues-Negative-21042012.tsv")
+classifiers_pos_gpc = collect_classifiers_gpc("gpc/GermanPolarityClues-Positive-21042012.tsv")
+
+
+def sentiws_eval(text):
+    sentiment_tokens = {}
+    sentiment_score = 0
+    for token in text.doc:
+        if token.is_stop is not True:
+            token_row = classifiers_neg_sentiws[classifiers_neg_sentiws.formen == token.text]
+            if not token_row.empty:
+                sentiment_tokens[token.text] = token_row["wert"].values[0]
+                sentiment_score += token_row["wert"].values[0]
+            else:
+                token_row = classifiers_pos_sentiws[classifiers_pos_sentiws.formen == token.text]
+                if not token_row.empty:
+                    sentiment_tokens[token.text] = token_row["wert"].values[0]
+                    sentiment_score += token_row["wert"].values[0]
+    sentiment_tokens["sentiment_score"] = sentiment_score
+    return sentiment_tokens
+
+
+def gpc_eval(text):
+    positive_token = []
+    negative_token = []
+    sentiment_tokens = {}
+    for token in text.doc:
+        if token.is_stop is not True:
+            token_row = classifiers_neg_gpc[classifiers_neg_gpc.form == token.text]
+            if not token_row.empty:
+                negative_token.append(token.text)
+            else:
+                token_row = classifiers_pos_gpc[classifiers_pos_gpc.form == token.text]
+                if not token_row.empty:
+                    positive_token.append(token.text)
+    sentiment_tokens["positiv"] = positive_token
+    sentiment_tokens["negativ"] = negative_token
+    return sentiment_tokens
+
+
+for f, protokoll in spacy_db.items():
+    for rede in protokoll:
+        rede.user_data["sentiws"] = sentiws_eval(rede)
+
+
+for f, protokoll in spacy_db.items():
+    for rede in protokoll:
+        rede.user_data["gpc"] = gpc_eval(rede)
+
+for f, d in spacy_db.items():
+    doc_bin = DocBin(
+        attrs=["POS", "TAG", "LEMMA", "IS_STOP", "DEP", "SHAPE", "ENT_ID", "ENT_IOB", "ENT_KB_ID", "ENT_TYPE"],
+        store_user_data=True)
+    for doc in d:
+        doc_bin.add(doc)
+    spacy_out = doc_bin.to_bytes()
+    with open(file=("plenarprotokolle/testing_prep/" + f + ".sentiment"), mode="wb") as spacy_outfile:
+        spacy_outfile.write(spacy_out)
+
 
